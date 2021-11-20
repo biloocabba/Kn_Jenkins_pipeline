349c401d1ca589af30285340deab23ca74ae334c
biloocabba <80125020+biloocabba@users.noreply.github.com>
2021-11-20 16:51:36 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -3,6 +3,8 @@ def buildApp() {
     echo 'The app is buileded now be happy!'
     sh'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash'
     sh'. ~/.nvm/nvm.sh'
+    sh'. ~/.profile'
+    sh'. ~/.bashrc'
     sh 'nvm  install 14.18.1'
 }
 
