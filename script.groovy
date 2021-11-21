ef26a4ac760956b5d7b3ef0bb4a7fd6ed336ca5d
biloocabba <80125020+biloocabba@users.noreply.github.com>
2021-11-21 11:37:48 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,6 +6,8 @@ def buildApp() {
     sh 'node -v'
     sh'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash'
     sh 'source ~/.nvm/nvm.sh'
+    sh 'export NVM_DIR="$HOME/.nvm'
+    sh '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm'
     sh 'nvm install 14.18.1'
     //sh 'npm install '
     //sh 'npm  start'
