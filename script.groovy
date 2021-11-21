e1ef9753797881fe0fa95adeafc9cf39ca4f5fa4
biloocabba <80125020+biloocabba@users.noreply.github.com>
2021-11-21 10:36:26 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,9 +1,6 @@
 def buildApp() {
     echo 'building the application'
     echo 'The app is buileded now be happy!'
-    sh'mkdir /usr/local/nvm'
-    sh'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh |NVM_DIR=/usr/local/nvm bash'
-    sh'. /usr/local/nvm/nvm.sh'
    
     sh 'nvm  install 14.18.1 -g'
 }
